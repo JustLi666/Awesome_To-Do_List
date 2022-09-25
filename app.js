0cdc238f9261ce70daad1f91c359ed0234a62ba5
Yinru Li <y_li28@uncg.edu>
2022-09-25 14:49:41 -0400

    added background image

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+})
+
+app.listen(3000, function(req, res) {
+  console.log("Server is running on port 3000.");
+})
