0cec155700ef75d0677b9e41ed750a1ee5747e9e
Yinru Li <y_li28@uncg.edu>
2022-10-11 17:44:36 -0400

    fixed no music served on express server

--- app.js
+++ app.js
@@ -8,34 +8,41 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-let mailTransporter = nodemailer.createTransport({
-  service:"gmail",
-  auth:{
-    user: "liyinru2000@gmail.com",
-    pass: "rlcozsjxwtviwxgx"
-  }
-});
 
-let details = {
-  from: "liyinru2000@gmail.com",
-  to: "y_li28@uncg.edu",
-  subject:"testing out nodemailer",
-  text:"You are smart and funny!"
-}
+app.use(express.static('public'));
 
-mailTransporter.sendMail(details, (err)=>{
-  if(err){
-    console.log("it has an error", err);
-  }
-  else{
-    console.log("email has been sent")
-  }
-})
+// let mailTransporter = nodemailer.createTransport({
+//   service:"gmail",
+//   auth:{
+//     user: "liyinru2000@gmail.com",
+//     pass: "rlcozsjxwtviwxgx"
+//   }
+// });
+//
+// let details = {
+//   from: "liyinru2000@gmail.com",
+//   to: "y_li28@uncg.edu",
+//   subject:"testing out nodemailer",
+//   text:"You are smart and funny!"
+// }
+//
+// mailTransporter.sendMail(details, (err)=>{
+//   if(err){
+//     console.log("it has an error", err);
+//   }
+//   else{
+//     console.log("email has been sent")
+//   }
+// })
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
-})
+  // res.sendFile(__dirname + "/MT1990 - Winter gloves（冬天的手套）.mp3");
+  // res.sendFile(__dirname + "/N.K.杨凯 - 深秋的雨.mp3");
+  // res.sendFile(__dirname + "/万能日记 - 风的小径.mp3");
+  // res.sendFile(__dirname + "/春野杉卉 - 静かな夏.mp3");
+});
 
 app.listen(3000, function(req, res) {
   console.log("Server is running on port 3000.");
-})
+});
