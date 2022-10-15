54b9e755ca5a889cf37ca8646cd17e077ec4c8f0
Yinru Li <y_li28@uncg.edu>
2022-10-15 16:29:31 -0400

    created user database management page

--- app.js
+++ app.js
@@ -2,15 +2,50 @@ const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
+const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 
-// app.use(bodyParser.json());
+
+let sql;
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static('public'));
-// app.post('api/register', async(req, res)=>{
-//   console.log(req.body);
-//   res.json({status: 'ok'})
+
+//connect database
+const db = new sqlite3.Database("./users.db", sqlite3.OPEN_READWRITE, (err)=>{
+  if (err) return console.error(err.message);
+});
+
+//create table
+// sql = 'CREATE TABLE users(id INTEGER PRIMARY KEY, username, password, email)';
+// db.run(sql);
+
+//drop table
+// db.run("DROP TABLE users");
+
+//insert data
+// sql = 'INSERT INTO users(username, password, email) VALUES (?,?,?)'
+// db.run(sql, ["Bruce Li", "123456", "liyinru2000@gmail.com"], (err)=>{
+//   if (err) return console.error(err.message);
 // });
+
+//delete data
+// sql = 'DELETE FROM users WHERE id=?';
+// db.run(sql, [1], (err)=>{
+//   if (err) return console.error(err.message);
+// });
+
+//query data
+sql = 'SELECT * FROM users';
+db.all(sql, [], (err, rows)=>{
+  if(err) return console.error(err.message);
+  rows.forEach(row=>{
+    console.log(row);
+  });
+});
 
 
 //
@@ -52,6 +87,10 @@ app.get("/signup", function(req, res) {
 
 app.get("/signin", function(req, res) {
   res.sendFile(__dirname + "/signin/signin.html");
+});
+
+app.get("/users", function(req, res) {
+  res.sendFile(__dirname + "/users.html");
 });
 
 app.listen(3000, function(req, res) {
