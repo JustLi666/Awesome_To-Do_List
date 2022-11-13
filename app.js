e3009df59fec9e4256947edf189c04116fd8125c
Kiki <H_Huerta@uncg.edu>
2022-11-12 20:51:03 -0500

    Henry setup MongoDB

--- app.js
+++ app.js
@@ -1,115 +1,50 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
-const sqlite3 = require('sqlite3').verbose();
 const bcrypt = require('bcrypt');
 const app = express();
 const passport = require('passport');
+const passportLocal = require('passport-local');
+const passportLocalMongoose = require("passport-local-mongoose");
 const flash = require('express-flash');
 const session = require('express-session');
 const methodOverride = require('method-override');
-
-
-
-const users = []; //will be replaced by database.
-
-const initializePassport = require('./passport-config');
-initializePassport(
-  passport,
-  username => users.find(user => user.username === username),
-  id => users.find(user => user.id === id)
-);
-
+const mongoose = require('mongoose');
+const User = require("./models/user");
+const ObjectId = require('mongodb').ObjectId;
+const alert = require('alert');
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({
   extended: false
 }));
 app.set('view-engine', 'ejs');
-app.use(flash());
+
+mongoose.connect("mongodb+srv://Li:258369AA@atdl.nxqeadw.mongodb.net/?retryWrites=true&w=majority", {
+  useNewUrlParser: true
+});
+
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: "Li is an idiot",
   resave: false,
   saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(methodOverride('_method'))
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
-// let sql;
-
-
-//connect database
-// const db = new sqlite3.Database("./users.db", sqlite3.OPEN_READWRITE, (err) => {
-//   if (err) return console.error(err.message);
-// });
-
-// create table
-// sql = 'CREATE TABLE users(id INTEGER PRIMARY KEY, username, password, email)';
-// db.run(sql);
-
-//drop table
-// db.run("DROP TABLE users");
-
-//insert data
-// sql = 'INSERT INTO users(username, password, email) VALUES (?,?,?)'
-// db.run(sql, ["Bruce Li", "123456", "liyinru2000@gmail.com"], (err)=>{
-//   if (err) return console.error(err.message);
-// });
-
-//delete data
-// sql = 'DELETE FROM users WHERE id=?';
-// db.run(sql, [1], (err)=>{
-//   if (err) return console.error(err.message);
-// });
-
-// query data
-// sql = 'SELECT * FROM users';
-// db.all(sql, [], (err, rows)=>{
-//   if(err) return console.error(err.message);
-//   rows.forEach(row=>{
-//     console.log(row);
-//   });
-// });
-
-
-//
-// let mailTransporter = nodemailer.createTransport({
-//   service:"gmail",
-//   auth:{
-//     user: "liyinru2000@gmail.com",
-//     pass: "rlcozsjxwtviwxgx"
-//   }
-// });
-//
-// let details = {
-//   from: "liyinru2000@gmail.com",
-//   to: "y_li28@uncg.edu",
-//   subject:"testing out nodemailer",
-//   text:"You are smart and funny!"
-// }
-//
-// mailTransporter.sendMail(details, (err)=>{
-//   if(err){
-//     console.log("it has an error", err);
-//   }
-//   else{
-//     console.log("email has been sent")
-//   }
-// })
+passport.use(new passportLocal(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 
-app.get("/", checkAuthenticated, function(req, res) {
+app.get("/", signedIn, function(req, res) {
   res.render("index.ejs");
 });
 
@@ -121,34 +56,51 @@ app.get("/signup", function(req, res) {
   res.render("signup.ejs");
 });
 
-app.post('/signup', async function(req, res) {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    users.push({
-      id: Date.now().toString(),
-      username: req.body.username,
-      password: hashedPassword
+app.post('/signup', function(req, res) {
+  User.register(new User({
+    username: req.body.username
+  }), req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      return res.render("signup.ejs");
+    }
+    passport.authenticate("local")(req, res, function() {
+      res.redirect("/signin");
     });
-    res.redirect('/signin');
-  } catch {
-    res.redirect('/signup');
-  }
-  console.log(users);
+  });
 });
 
 app.get("/signin", function(req, res) {
   res.render("signin.ejs");
 });
 
-app.post('/signin', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/signin',
-  failureFlash: true
-}));
+app.post('/signin', passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/signin"
+}), function(req, res) {});
 
-app.get("/users", function(req, res) {
-  res.render("users.ejs", {username: "Bruce", password: 111111});
+app.get("/logout", function(req, res, next) {
+  req.logout(function(err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
 });
+
+function signedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/signin");
+}
+
+// app.get("/users", function(req, res) {
+//   res.render("users.ejs", {
+//     username: "Bruce",
+//     password: 111111
+//   });
+// });
 
 app.get("/admin", function(req, res) {
   res.render("admin.ejs");
@@ -156,31 +108,43 @@ app.get("/admin", function(req, res) {
 
 app.post("/admin", function(req, res) {
   if (req.body.code == 123456) {
-    res.redirect("/users");
-  } else{
+    res.render("test.ejs");
+  } else {
     res.redirect('/admin');
   }
 });
 
-app.get("/test", function(req, res){
-  res.render("test.ejs", {
-    listTitle: "User Accounts",
-    newItem: users
-  });
+
+// CRUD api
+
+app.get('/guys', async function(req, res) {
+  const guys = await User.find({});
+
+  try {
+    res.send(guys);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-app.delete('/logout', (req, res) => {
-  req.logOut(function() {
-    res.redirect('/signin')
-  })
-})
+// delete user account
+app.post('/guys', function(req, res) {
+  User.findByIdAndDelete(req.body.userID, function(err, docs) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("Deleted : ", docs);
+    }
+  });
+  res.render("test.ejs");
+});
 
-function checkAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next()
-  }
-  res.redirect('/signin')
-}
+// show creation time
+app.post('/times', function(req, res) {
+    alert(ObjectId(req.body.userID).getTimestamp().toLocaleDateString() + " " + ObjectId(req.body.userID).getTimestamp().toLocaleTimeString());
+    res.render("test.ejs");
+});
+
 
 app.listen(3000, function(req, res) {
   console.log("Server is running on port 3000.");
